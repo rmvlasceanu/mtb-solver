c3909918db32dbbd199611b915c054d0b3749fce
Razvan Vlasceanu <razvan.vlasceanu@ro.ibm.com>
2017-03-23 17:19:43 +0200

    added component dependencies into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MtbInputComponent } from './mtb-input/mtb-input.component';
+import { MtbCellComponent } from './mtb-cell/mtb-cell.component';
+import { MtbStepComponent } from './mtb-step/mtb-step.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MtbInputComponent,
+    MtbCellComponent,
+    MtbStepComponent
   ],
   imports: [
     BrowserModule,
