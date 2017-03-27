a1099565443eff74c2b3efbba1156f8a14dcd003
Razvan Vlasceanu <razvan.vlasceanu@ro.ibm.com>
2017-03-27 15:11:43 +0300

    removed sources

--- src/app/helpers/Cell.ts
+++
@@ -1,9 +0,0 @@
-import {MTBCellType} from "./mtb-cell-type.enum";
-/**
- * Created by razvanvlasceanu on 3/13/17.
- */
-export class Cell{
-  constructor(public cellType: MTBCellType = MTBCellType.Void ){
-
-  }
-}
